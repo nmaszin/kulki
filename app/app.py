59388cca8b165af35628aad64f9da4f49cad2a09
qwercik <erykandrzejewski@gmail.com>
2020-05-13 18:07:51 +0200

    Modify FPS at runtime

--- app/app.py
+++ app/app.py
@@ -12,6 +12,8 @@ class App:
   paused = False
 
   def __init__(self):
+    self.fps = self.FPS
+
     pygame.init()
     pygame.display.set_caption(self.WINDOW_TITLE)
     self.surface = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
@@ -22,7 +24,7 @@ class App:
     pygame.display.update()
 
     self.UPDATE_SIMULATION = pygame.USEREVENT
-    pygame.time.set_timer(self.UPDATE_SIMULATION, int(1000 / self.FPS))
+    pygame.time.set_timer(self.UPDATE_SIMULATION, int(1000 / self.fps))
 
   def run(self):
     self.running = True
@@ -41,13 +43,22 @@ class App:
         self.running = False
       elif event.key == pygame.K_p:
         if self.paused:
-          pygame.time.set_timer(self.UPDATE_SIMULATION, int(1000 / self.FPS))
+          pygame.time.set_timer(self.UPDATE_SIMULATION, int(1000 / self.fps))
         else:
           pygame.time.set_timer(self.UPDATE_SIMULATION, 0)
         
         self.paused = not self.paused
+      elif event.key == pygame.K_a:
+        self.fps -= 10
+        print(self.fps)
+        pygame.time.set_timer(self.UPDATE_SIMULATION, int(1000 / self.fps))
+      elif event.key == pygame.K_d:
+        self.fps += 10
+        print(self.fps)
+        pygame.time.set_timer(self.UPDATE_SIMULATION, int(1000 / self.fps))
+
     elif event.type == self.UPDATE_SIMULATION:
-      self.simulation.update(1 / self.FPS)
+      self.simulation.update(1 / self.fps)
 
       self.surface.fill(self.WINDOW_BACKGROUND)
       self.simulation.draw(self.surface)
