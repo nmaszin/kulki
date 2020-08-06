0d9047617e1fd9addb48303a0ea13111373ea0fb
qwercik <erykandrzejewski@gmail.com>
2020-08-06 13:25:52 +0200

    Add circle class

---
+++ app/graphics/circle.py
@@ -0,0 +1,19 @@
+import pygame
+import pygame.gfxdraw
+
+class Circle:
+    def __init__(self, point, radius, color):
+        self.point = point
+        self.radius = radius
+        self.color = color
+    
+    def draw(self, surface):
+        params = (
+            surface,
+            *tuple(map(int, self.point)),
+            self.radius,
+            self.color
+        )
+
+        pygame.gfxdraw.aacircle(*params)
+        pygame.gfxdraw.filled_circle(*params)
